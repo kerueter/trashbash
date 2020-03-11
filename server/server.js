e0734ba336f2cda939270bd44777bb8ab90c630b
Kevin Rueter <k.rueter93@gmail.com>
2020-03-11 17:40:45 +0100

    added server endpoint for images uploads and added radius filtering of trash markers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-var app = express();
-var expressWs = require('express-ws')(app);
-const { Client } = require('pg')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const expressWs = require('express-ws')(app);
+const fileUpload = require('express-fileupload');
+const crypto = require('crypto');
+const { Client } = require('pg');
 
 const client = new Client({
   user: 'marcel',
@@ -15,6 +17,11 @@ client.connect();
 
 app.use(cors());
 app.use(express.json({limit: '5mb'}));
+app.use('/media', express.static(__dirname + '/media'));
+app.use(fileUpload({
+	useTempFiles : true,
+	tempFileDir : '/tmp/'
+}));
 
 app.listen(1338, function () {
   console.log('Example app listening on port 1338!');
@@ -27,15 +34,38 @@ var regWs = expressWs.getWss('/trash');
 app.options('*', cors());
 
 app.get('/trash', cors(), (req, res) => {
-  let queryMessage = 'SELECT * FROM trash ORDER BY id ASC';
+  const userLocationLat = req.query.userLocationLat;
+  const userLocationLng = req.query.userLocationLng;
+  const radiusKm = req.query.radiusKm;
+
+  // check if query parameters are set
+  if (!userLocationLat || !userLocationLng || !radiusKm) {
+    res.status(400).send("User location or radius request missing in query parameters.");
+    return;
+  }
+
+  const queryMessage = 'SELECT * FROM trash ORDER BY id ASC';
   client.query(queryMessage, (err, dbRes) => {
     if(err) {
       res.status(500).send("Error");
       return;
     }
-    res.status(200).send(dbRes.rows);
+
+    // check if trash location is in requested radius of user
+    const validRows = [];
+    dbRes.rows.forEach(dbRow => {
+      const distance = getTrashDistance(
+        { latitude: userLocationLat, longitude: userLocationLng},
+        { latitude: dbRow.latitude, longitude: dbRow.longitude}
+      );
+      if (distance < radiusKm) {
+        validRows.push(dbRow);
+      }
+    });
+    res.status(200).send(validRows);
   });
 });
+
 app.post('/trash', cors(), (req, res) => {
   const trashReport = req.body;
   const queryText = `INSERT INTO trash (time,username,latitude,longitude,hausmuell,gruenabfall,sperrmuell,sondermuell,photo) VALUES (to_timestamp($1), $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`;
@@ -52,3 +82,37 @@ app.post('/trash', cors(), (req, res) => {
     res.status(200).send(dbRes.rows[0]);
   });
 });
+
+app.post('/trash/images/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+	Object.keys(req.files).forEach(fileKey => {
+    const hash = crypto.createHash('md5').update(crypto.randomBytes(16)).digest('hex');
+    const fileLocation = `/media/uploads/${hash}.jpg`;
+		req.files[fileKey].mv(`./${fileLocation}`, (err) => {
+			if (err) {
+				return res.status(500).send(err);
+			}
+		});
+	});
+	res.status(200).send({
+    'Location': fileLocation
+  });
+});
+
+/**
+ * Get the distance of the trash location to the radius center (in km)
+ *
+ * @param center Geo location of the radius center
+ * @param marker Geo location of the trash marker
+ */
+function getTrashDistance(center, marker) {
+  const ky = 40000 / 360;
+  const kx = Math.cos(Math.PI * center.latitude / 180.0) * ky;
+  const dx = Math.abs(center.longitude - marker.longitude) * kx;
+  const dy = Math.abs(center.latitude - marker.latitude) * ky;
+
+  return Math.sqrt(dx * dx + dy * dy);
+}
